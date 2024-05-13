075a968e00d4de7d3c03e18c61fb36d3e10dfc1e
IshimweJoliePrincesse <148842543+IshimweJoliePrincesse@users.noreply.github.com>
2024-05-13 21:19:11 +0200

    backend and frontend of a real time chat app

---
+++ src/ChatsPage.jsx
@@ -0,0 +1,21 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    " dcfb7db3-b998-4953-9047-48ffc4b2de2d",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100vh" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
